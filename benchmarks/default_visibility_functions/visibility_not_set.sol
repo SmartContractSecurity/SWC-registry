8187124409285dc2ec1cc42d273207f1ad40ac4c
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-09-07 11:19:21 +0200

    Update visibility benchmark

---
+++ benchmarks/default_visibility_functions/visibility_not_set.sol
@@ -0,0 +1,15 @@
+/*
+ * @source: https://blog.sigmaprime.io/solidity-security.html
+ * @author: SigmaPrime 
+ */
+
+pragma solidity 0.4.24;
+
+contract VisibilityNotSet {
+
+    function visibilityNotSet() returns (bool) {
+ 		return true;
+     }
+}
+
+
