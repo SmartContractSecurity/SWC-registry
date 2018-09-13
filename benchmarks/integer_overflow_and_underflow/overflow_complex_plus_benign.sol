ebbd0d927427db9acfea4412093641ff865f0078
thec00n <thec00n@fork.at>
2018-09-13 16:38:23 +0800

    new issue format

---
+++ benchmarks/integer_overflow_and_underflow/overflow_complex_plus_benign.sol
@@ -0,0 +1,31 @@
+/*
+ * @source: https://github.com/ConsenSys/smart-contract-best-practices
+ * @author: Consensys Diligence
+ */
+
+pragma solidity 0.4.24;
+
+contract Overflow_simple_plus_bengin {
+	
+	mapping (address => uint256) public balanceOf;
+
+	// INSECURE
+	function transfer1(address _to, uint256 _value) public {
+	    /* Check if sender has balance */
+	    require(balanceOf[msg.sender] >= _value);
+	    /* Add and subtract new balances */
+	    balanceOf[msg.sender] -= _value;
+	    balanceOf[_to] += _value;
+	}
+
+	// SECURE
+	function transfer2(address _to, uint256 _value) public {
+	    /* Check if sender has balance and for overflows */
+	    require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
+
+	    /* Add and subtract new balances */
+	    balanceOf[msg.sender] -= _value;
+	    balanceOf[_to] += _value;
+	}
+}
+
