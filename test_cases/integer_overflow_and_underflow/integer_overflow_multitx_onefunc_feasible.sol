913fb50d8a6ae60bc05b2f2fb6185e741bd8a978
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-09-18 09:50:45 +0700

    Add some of Suhabe's multi-tx benchmarks

---
+++ test_cases/integer_overflow_and_underflow/integer_overflow_multitx_onefunc_feasible.sol
@@ -0,0 +1,23 @@
+/*
+ * @source: https://github.com/ConsenSys/evm-analyzer-benchmark-suite
+ * @author: Suhabe Bugrara
+ */
+
+//Multi-transactional, single function
+//Arithmetic instruction reachable
+
+pragma solidity ^0.4.23;
+
+contract IntegerOverflowMultiTxOneFuncFeasible {
+    uint256 private initialized = 0;
+    uint256 public count = 1;
+
+    function run(uint256 input) public {
+        if (initialized == 0) {
+            initialized = 1;
+            return;
+        }
+
+        count -= input;
+    }
+}
---
+++ test_cases/integer_overflow_and_underflow/integer_overflow_multitx_onefunc_infeasible.sol
@@ -0,0 +1,22 @@
+/*
+ * @source: https://github.com/ConsenSys/evm-analyzer-benchmark-suite
+ * @author: Suhabe Bugrara
+ */
+
+//Multi-transactional, single function
+//Overflow infeasible because arithmetic instruction not reachable
+
+pragma solidity ^0.4.23;
+
+contract IntegerOverflowMultiTxOneFuncInfeasible {
+    uint256 private initialized = 0;
+    uint256 public count = 1;
+
+    function run(uint256 input) public {
+        if (initialized == 0) {
+            return;
+        }
+
+        count -= input;
+    }
+}
