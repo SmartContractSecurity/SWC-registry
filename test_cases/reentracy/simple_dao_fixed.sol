c78ea62e715b7d1d301f264099048acbdb4677b4
Josselin <josselin@trailofbits.com>
2018-09-19 09:27:56 +0100

    DAO examples modification: - Add payable attribute to donate to make the contract exploitable - Add pragma version (based on their json) - Add public attribute to avoid SWC-100 - Add view and require to avoid solc warnings

--- test_cases/reentracy/simple_dao_fixed.sol
+++ test_cases/reentracy/simple_dao_fixed.sol
@@ -1,24 +1,25 @@
 /*
  * @source: http://blockchain.unica.it/projects/ethereum-survey/attacks.html#simpledao
  * @author: Atzei N., Bartoletti M., Cimoli T
- * Modified by Bernhard Mueller
+ * Modified by Bernhard Mueller, Josselin Feist
  */
+pragma solidity 0.4.24;
 
 contract SimpleDAO {
   mapping (address => uint) public credit;
     
-  function donate(address to) {
+  function donate(address to) payable public{
     credit[to] += msg.value;
   }
     
-  function withdraw(uint amount) {
+  function withdraw(uint amount) public {
     if (credit[msg.sender]>= amount) {
       credit[msg.sender]-=amount;
-      msg.sender.call.value(amount)();
+      require(msg.sender.call.value(amount)());
     }
   }  
 
-  function queryCredit(address to) returns (uint){
+  function queryCredit(address to) view public returns (uint){
     return credit[to];
   }
 }
