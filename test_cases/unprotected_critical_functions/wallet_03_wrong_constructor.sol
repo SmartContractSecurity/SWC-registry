7cfb71128f2d48f1a0839c03f49ccf6571208709
Bernhard Mueller <b-mueller@users.noreply.github.com>
2018-11-16 15:04:32 +0700

    Add various examples for Ether withdrawal (#118)
    * Add various examples for Ether withdrawal
    
    * Fix typo
    
    * Clarify description

---
+++ test_cases/unprotected_critical_functions/wallet_03_wrong_constructor.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.24;
+
+/* User can add pay in and withdraw Ether.
+   The constructor is wrongly named, so anyone can become 'creator' and withdraw all funds.
+*/
+
+contract Wallet {
+    address creator;
+    
+    mapping(address => uint256) balances;
+
+    function initWallet() public {
+        creator = msg.sender;
+    }
+
+    function deposit() public payable {
+    	assert(balances[msg.sender] + msg.value > balances[msg.sender]);
+        balances[msg.sender] += msg.value;
+    }
+    
+    function withdraw(uint256 amount) public {
+        require(amount <= balances[msg.sender]);
+        msg.sender.transfer(amount);
+        balances[msg.sender] -= amount;
+    }
+
+    // In an emergency the owner can migrate  allfunds to a different address.
+
+    function migrateTo(address to) public {
+        require(creator == msg.sender);
+        to.transfer(this.balance);
+    }
+
+}
---
+++ test_cases/unprotected_critical_functions/wallet_04_confused_sign.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.24;
+
+/* User can add pay in and withdraw Ether.
+   Unfortunatelty, the developer was drunk and used the wrong comparison operator in "withdraw()"
+   Anybody can withdraw arbitrary amounts of Ether :()
+*/
+
+contract Wallet {
+    address creator;
+    
+    mapping(address => uint256) balances;
+
+    constructor() public {
+        creator = msg.sender;
+    }
+
+    function deposit() public payable {
+    	assert(balances[msg.sender] + msg.value > balances[msg.sender]);
+        balances[msg.sender] += msg.value;
+    }
+    
+    function withdraw(uint256 amount) public {
+        require(amount >= balances[msg.sender]);
+        msg.sender.transfer(amount);
+        balances[msg.sender] -= amount;
+    }
+
+    // In an emergency the owner can migrate  allfunds to a different address.
+
+    function migrateTo(address to) public {
+        require(creator == msg.sender);
+        to.transfer(this.balance);
+    }
+
+}
