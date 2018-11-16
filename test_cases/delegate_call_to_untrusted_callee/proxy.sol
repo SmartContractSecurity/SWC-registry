450e9b475a6cf9720a68c07f1922cc0f49ca8742
thec00n <thec00n@fork.at>
2018-11-16 13:39:14 +0800

    make it simpler

--- test_cases/delegate_call_to_untrusted_callee/proxy.sol
+++ test_cases/delegate_call_to_untrusted_callee/proxy.sol
@@ -3,15 +3,9 @@ pragma solidity ^0.4.24;
 contract Proxy {
 
   address owner;
-  address callee;
 
-  constructor() {
+  constructor() public {
     owner = msg.sender;  
-  	callee = address(0x0);
-  }
-
-  function setCallee(address newCallee) public {
-  	callee = newCallee;
   }
 
   function forward(address callee, bytes _data) public {
