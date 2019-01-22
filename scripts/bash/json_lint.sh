a1d035948d7403e17c9f1cb9dc2ca5baf347968e
Aleksandr Sobolev <thesobolev@gmail.com>
2019-01-23 00:05:08 +0700

    Update json_lint checker

--- scripts/bash/json_lint.sh
+++ scripts/bash/json_lint.sh
@@ -1,5 +1,5 @@
-find ../../test_cases -name '*.json' -print | while read line; do
-    command=$(../node_modules/jsonlint/lib/cli.js $line > /dev/null)
+find ../test_cases -name '*.json' -print | while read line; do
+    command=$(./node_modules/jsonlint/lib/cli.js $line > /dev/null)
     if (($? > 0)); then
         echo "ERROR: Wrong json object - $line"
         exit 1
