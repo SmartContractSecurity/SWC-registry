6d789a65683b33eea6e9a5ada10f1fa1d5f7c09f
Aleksandr Sobolev <thesobolev@gmail.com>
2019-02-07 15:15:09 +0700

    Display errors in configs in readble format

--- scripts/yaml_validate.js
+++ scripts/yaml_validate.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 const web3 = require('web3');
+const chalk = require('chalk');
 
 let files = [];
 
@@ -25,16 +26,44 @@ const HASH_PREFIX_LENGTH = "0x".length;
 
 const ERRORS = {};
 
+const CHECKER_ERRORS = {
+    DESCRIPTION: {
+        code: '100',
+        success: 'Description is ok'
+    },
+    SWC_ID_FORMAT: {
+        code: '101',
+        success: 'Valid SWC-ID format'
+    },
+    ISSUE_COUNT: {
+        code: '102',
+        success: 'Issue count value is correct',
+    },
+    HASH_GENERATION: {
+        code: '103',
+        success: 'Hashes was successfuly generated'
+    },
+    HASH_LENGTH: {
+        code: '104',
+        success: 'Valid hash length'
+    },
+    HASH_PREFIX: {
+        code: '105',
+        success: 'Hash prefix 0x exists'
+    },
+    HASH_COMPARE: {
+        code: '106',
+        success: 'Hash was generated regarding creation (or runtime) bytecode in any of contracts at test case json'
+    },
+    LINO_POINTER: {
+        code: '107',
+        success: 'Issue location points to nonexistent file'
+    }
+}
+
 
 const logError = (config, code, error) => {
-    if (!config) {
-        console.log(code)
-        console.log(error);
-        console.log('testsasfasfasdf')
-    }
-    if (Object.keys(ERRORS).indexOf(config) < 0) {
-        ERRORS[config] = {};
-    }
+    if (Object.keys(ERRORS).indexOf(config) < 0) ERRORS[config] = {};
     ERRORS[config][code] = error
 }
 
@@ -103,7 +132,9 @@ const hashValidator = (config, hash) => {
     const creation = contractKeys.map(contract => generateHash(contracts[contract]['bin'], config));
     const runtime = contractKeys.map(contract => generateHash(contracts[contract]['bin-runtime'], config));
     if(!creation.includes(hash) && !runtime.includes(hash)) {
-        logError(config, 106, `Wrong hash! ${hash}, Possible variants: ${runtime} \n ${creation}`);
+        const displayCreation = creation.join('\n-')
+        const displayRuntime = runtime.join('\n-')
+        logError(config, 106, `Wrong hash! Possible variants:\n ${displayCreation} \n ${displayRuntime}`);
     }
 };
 
@@ -136,11 +167,33 @@ const validateYamlConfig = () => {
 
 validateYamlConfig();
 
+const displayErrors = () => {
+    console.log(chalk.white.bgRed('ERRORS:'))
+    const configs = Object.keys(ERRORS);
+    configs.map(config => {
+        console.log('*----------------------------------------------*')
+        console.log(chalk.red.bold(`Config: ${config}`));
 
+        const errors = Object.keys(ERRORS[config]);
+        const checkers = Object.keys(CHECKER_ERRORS);
+
+        checkers.map(key => {
+            const check = CHECKER_ERRORS[key];
+            const { code, success } = check;
+            let displayCheck = chalk.green(` -- ${success}`);
+            if (errors.includes(code)) {
+                const error = ERRORS[config][code];
+                displayCheck = chalk.red(` -- ${error}`);
+            }
+            console.log(displayCheck)
+        })
+        console.log('*----------------------------------------------*')
+    })
+};
 if (Object.keys(ERRORS).length > 0) {
-    console.log(JSON.stringify(ERRORS, null, 2))
+    displayErrors();
     process.exit(1);
 } else {
-    console.log('Passed.')
+    console.log(chalk.green.bold('Success! \nYaml configs were validated. All configs passed required checks.'));
     process.exit();
 }
