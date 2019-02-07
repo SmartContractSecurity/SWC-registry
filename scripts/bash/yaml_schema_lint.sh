b6823bad6ebbfdd31d8a7fb81f36a9766719be4b
Aleksandr Sobolev <thesobolev@gmail.com>
2019-02-07 15:32:27 +0700

    Update yaml schema linter

--- scripts/bash/yaml_schema_lint.sh
+++ scripts/bash/yaml_schema_lint.sh
@@ -1,9 +1,13 @@
 find ../test_cases -name '*.yaml' -print | while read line; do
-    command=$(./node_modules/yaml-schema-validator/index.js validate -s ../swc-config-schema.yaml -f $line)
+    command=$(./node_modules/yaml-schema-validator/index.js validate -s swc-config-schema.yaml -f $line)
     if [[ $command = *"ERROR : ====== Schema Validation Error ======"* ]]; then
         echo "$command" >&2
-        exit 1
-    else
-        echo "SUCCESS: $line"
+        echo ""
+        export VALIDATION_ERROR=1
     fi
 done
+
+if [ -z "$VALIDATION_ERROR" ]
+then
+    exit 1
+fi
