9b1a8ea8a2b2a3882cf7520ead3f70665f343cae
andrea zuccarini <andrearanma@gmail.com>
2019-02-28 10:03:07 +0100

    add 0x padding and manage multi bytecode pairs.

--- scripts/utils.js
+++ scripts/utils.js
@@ -25,7 +25,7 @@ const printFinalOutput = (fileName, bin, binSha3, binDisasm, binRuntime, binRunt
 }
 
 const getSha3 = (bytecode) => {
-  return web3.utils.sha3(bytecode);
+  return web3.utils.sha3('0x' + bytecode);
 }
 
 const getDisasm = (bytecode) => {
@@ -44,35 +44,39 @@ const getDisasm = (bytecode) => {
 
 const checkByteCode = () => {
   if(!command) {
-    console.log('You should enter a path to a compile artifact')
+    console.log('You should enter a path to a compile artifact.')
     process.exit(1);
   }
   let artefact = JSON.parse(fs.readFileSync(command, 'utf8'));
-  let fileName = Object.keys(artefact.contracts)[0];
-  if(fileName) {
-    let bin = artefact.contracts[fileName].bin;
-    let binRuntime = artefact.contracts[fileName]['bin-runtime'];
-    if(bin && binRuntime) {
-      let binSha3 = getSha3(bin);
-      let binRuntimeSha3 = getSha3(binRuntime);
-      if(!binSha3 || !binRuntimeSha3) {
-        console.log('You should check if geth is running and the correctness of bin/bin-runtime in compile artifact');
-        process.exit(1);
+  let numberBytecodesPair = Object.keys(artefact.contracts);
+  if(numberBytecodesPair.length == 0) {
+    console.log('There are no bin and bin-runtime into compile artifact.');
+    process.exit(1);
+  }
+  for(let i=0;i<numberBytecodesPair.length;i++) {
+    let fileName = Object.keys(artefact.contracts)[i];
+    if(fileName) {
+      let bin = artefact.contracts[fileName].bin;
+      let binRuntime = artefact.contracts[fileName]['bin-runtime'];
+      if(bin && binRuntime) {
+        let binSha3 = getSha3(bin);
+        let binRuntimeSha3 = getSha3(binRuntime);
+        if(!binSha3 || !binRuntimeSha3) {
+          console.log('You should check the correctness of bin/bin-runtime into compile artifact.');
+          process.exit(1);
+        }
+        let binDisasm = getDisasm(bin);
+        let binRuntimeDisasm = getDisasm(binRuntime);
+        printFinalOutput(fileName, bin, binSha3, binDisasm, binRuntime, binRuntimeSha3, binRuntimeDisasm);
       }
-      let binDisasm = getDisasm(bin);
-      let binRuntimeDisasm = getDisasm(binRuntime);
-      printFinalOutput(fileName, bin, binSha3, binDisasm, binRuntime, binRuntimeSha3, binRuntimeDisasm);
-
+      else {
+        console.log('\nbin and bin-runtime missing in ' + fileName);
+      }
     }
     else {
-      console.log('bin or bin-runtime missing in compile artifact.');
+      console.log('You should insert a valid compile artefact.');
       process.exit(1);
     }
-
-  }
-  else {
-    console.log('You should insert a valid compile artefact');
-    process.exit(1);
   }
 }
 
