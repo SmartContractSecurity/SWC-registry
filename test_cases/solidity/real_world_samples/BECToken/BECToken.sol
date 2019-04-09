ba93f9668ec56d792c175261eefe0fff43c80dd8
Aleksandr Sobolev <thesobolev@gmail.com>
2019-04-09 12:08:15 +0600

    Change assert to require

--- test_cases/solidity/real_world_samples/BECToken/BECToken.sol
+++ test_cases/solidity/real_world_samples/BECToken/BECToken.sol
@@ -7,25 +7,25 @@ pragma solidity ^0.4.16;
 library SafeMath {
   function mul(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a * b;
-    assert(a == 0 || c / a == b);
+    require(a == 0 || c / a == b);
     return c;
   }
 
   function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // require(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    // require(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
   function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
+    require(b <= a);
     return a - b;
   }
 
   function add(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a + b;
-    assert(c >= a);
+    require(c >= a);
     return c;
   }
 }
