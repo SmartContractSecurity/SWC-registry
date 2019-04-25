a34b889f7263d530dd68593af9e3b30349fb9a10
biltz <corruptx@mail.ru>
2019-04-25 11:46:43 +0600

    Merged var use case to deprecated_simple.sol, created deprecated_simple_fixed.sol, fixed deprecated_simple.yaml, regenerated snapshots. Removed separate var usage use- and fix- cases.

--- test_cases/solidity/deprecated_constructs/deprecated_untyped_variable_declaration/deprecated_untyped_variable_declaration.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.25;
-
-contract DeprecatedVar {
-    function useVar() public view {
-        var a = [1,2,3];
-
-        var (x, y, z) = (false, "test", 0);
-    }
-}
--- test_cases/solidity/deprecated_constructs/deprecated_untyped_variable_declaration_fixed/deprecated_untyped_variable_declaration_fixed.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.25;
-
-contract DeprecatedVarFixed {
-    function useVar() public view {
-        uint8[3] memory a = [1,2,3];
-
-        (bool x, string memory y, uint8 z) = (false, "test", 0);
-    }
-}
