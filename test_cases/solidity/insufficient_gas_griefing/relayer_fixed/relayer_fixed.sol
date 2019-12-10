0507a7d1d2d23e4af0a5d9886452e8fbc6bccca0
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-12-10 09:44:28 -0800

    Insufficient gas griefing

---
+++ test_cases/solidity/insufficient_gas_griefing/relayer_fixed/relayer_fixed.sol
@@ -0,0 +1,28 @@
+/*
+ * @source: https://consensys.github.io/smart-contract-best-practices/known_attacks/#insufficient-gas-griefing
+ * @author: ConsenSys Diligence
+ * Modified by Kaden Zipfel
+ */
+
+pragma solidity ^0.4.21;
+
+contract Relayer {
+    mapping (bytes => bool) executed;
+
+    function relay(bytes _data) public {
+        // replay protection; do not call the same transaction twice
+        require(executed[_data] == false);
+        executed[_data] = true;
+
+        address executor = new Executor();
+        executor.call(bytes4(keccak256("execute(bytes)")), _data);
+    }
+}
+
+// Contract called by Relayer
+contract Executor {
+    function execute(bytes _data, uint _gasLimit) public {
+        require(gasleft() >= _gasLimit);
+        // Execute contract code
+    }
+}
