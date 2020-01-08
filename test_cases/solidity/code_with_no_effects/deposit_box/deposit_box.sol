7702cbe753ae5a2d2a5eedbf8cb938680beda48d
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-01-08 09:40:45 -0800

    Add example

---
+++ test_cases/solidity/code_with_no_effects/deposit_box/deposit_box.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract DepositBox {
+    mapping(address => uint) balance;
+
+    // Accept deposit
+    function deposit(uint amount) public payable {
+        require(msg.value == amount, 'incorrect amount');
+        // Should update user balance
+        balance[msg.sender] == amount;
+    }
+}
