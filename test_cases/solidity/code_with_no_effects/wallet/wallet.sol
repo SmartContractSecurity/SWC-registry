0c4941a34cbc09835f23efe384e8dda1e6a534f1
Bernhard Mueller <b-mueller@users.noreply.github.com>
2020-03-18 03:35:11 +0100

    SWC 135 - Numbering corrected (#243)
    * Add swc-135 entry
    
    * Add swc-135 test cases
    
    * Make requested changes to entry
    
    * Add example
    
    * Change to swc-134
    
    * Side effects -> intended effects
    
    Co-authored-by: Kaden Zipfel <kaden.zipfel@hotmail.com>

---
+++ test_cases/solidity/code_with_no_effects/wallet/wallet.sol
@@ -0,0 +1,26 @@
+/*
+ * @author: Kaden Zipfel
+ */
+
+pragma solidity ^0.5.0;
+
+contract Wallet {
+    mapping(address => uint) balance;
+
+    // Deposit funds in contract
+    function deposit(uint amount) public payable {
+        require(msg.value == amount, 'msg.value must be equal to amount');
+        balance[msg.sender] = amount;
+    }
+
+    // Withdraw funds from contract
+    function withdraw(uint amount) public {
+        require(amount <= balance[msg.sender], 'amount must be less than balance');
+
+        uint previousBalance = balance[msg.sender];
+        balance[msg.sender] = previousBalance - amount;
+
+        // Attempt to send amount from the contract to msg.sender
+        msg.sender.call.value(amount);
+    }
+}
