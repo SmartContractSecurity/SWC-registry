52031608b67cc59b4d3c5ad141bf89de6b5bb5f4
Dominik Muhs <dmuhs@protonmail.ch>
2023-07-23 19:01:38 +0200

    Add code highlighting, clean up test cases

--- test_cases/solidity/integer_overflow_and_underflow/overflow_simple_add/overflow_simple_add.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-
-contract Overflow_Add {
-    uint public balance = 1;
-
-    function add(uint256 deposit) public {
-        balance += deposit;
-    }
-}
--- test_cases/solidity/unprotected_critical_functions/simple_suicide/simple_suicide.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.22;
-
-contract SimpleSuicide {
-
-  function sudicideAnyone() {
-    selfdestruct(msg.sender);
-  }
-
-}
